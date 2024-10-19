dcb49b68e59cf4b85554c9f9c321b1ac096fc2af
Allyssa Dalton <allyssamarie06@icloud.com>
2024-10-19 15:38:58 -0400

    Create CandyMonsters.h

---
+++ CandyMonsters.h
@@ -0,0 +1,36 @@
+#ifndef CANDYMONSTERS_H
+#define CANDYMONSTERS_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class CandyMonster {
+public:
+    string name;
+    int health;
+    int attack;
+    int defense;
+
+    CandyMonster(string n, int h, int a, int d); //constructor
+
+    //~CandyMonster(); //destructor
+
+    void displayInfo();
+
+    void resetHealth();
+
+    void decreaseHealth(int h);
+
+    bool isDefeated();
+
+    int getHealth();
+
+    int getAttack();
+
+    int getDefense();
+
+    
+};
+
+#endif
