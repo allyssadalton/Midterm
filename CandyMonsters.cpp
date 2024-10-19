34030cc9aa1a5c335a6a02189d81e962f6230dc3
Allyssa Dalton <allyssamarie06@icloud.com>
2024-10-19 15:38:46 -0400

    Create CandyMonsters.cpp

---
+++ CandyMonsters.cpp
@@ -0,0 +1,34 @@
+#include "CandyMonsters.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+CandyMonster::CandyMonster(string n, int h, int a, int d){
+    name = n;
+    health = h;
+    attack = a;
+    defense = d;
+}
+//CandyMonster::~CandyMonster(){}
+
+void CandyMonster::displayInfo(){
+    cout << "Monster: " << name << endl;
+    cout << "Health: " << health << endl;
+    cout << "Attack: " << attack << endl;
+    cout << "Defense: " << defense << endl;
+}
+
+void CandyMonster::decreaseHealth(int h){
+    health -= h;
+}
+
+void CandyMonster::resetHealth(){health = 10;}
+
+int CandyMonster::getHealth(){return health;}
+
+int CandyMonster::getAttack(){return attack;}
+
+int CandyMonster::getDefense(){return defense;}
+
+bool CandyMonster::isDefeated(){return health <= 0;}
